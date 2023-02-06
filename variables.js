22a34b5795e000bfbf44a5bf27338c5b622c897e
t9uo <t9uo@fresnou.org>
2023-02-06 09:52:36 -0800

    initial commit

---
+++ variables.js
@@ -0,0 +1,42 @@
+let myVariable="value";
+let myNewVariable=9;
+let myBool=true;
+
+//aaaaaaaaaaaaaaaaaa
+console.log(myVariable);
+console.log(myNewVariable + 10);
+console.log(myBool && true);
+
+//changes
+myNewVariable = 20;
+console.log(myNewVariable);
+
+//increase by 100
+myNewVariable = myNewVariable + 100;
+console.log(myNewVariable);
+
+//decrease by 10
+myNewVariable = myNewVariable - 34
+console.log(myNewVariable)
+
+//divide by 4...
+myNewVariable = myNewVariable /4
+console.log(myNewVariable)
+
+// 10/2 = 5 with 0 remainder
+myNewVariable = myNewVariable % 2;
+console.log(myNewVariable);
+
+//worksheet
+let x = 5;
+let y = 7;
+x=x + y;
+console.log(y)
+y=x + y;
+console.log(x<y);
+
+let secret = 10;
+console.log(secret)
+let guess = 5;
+console.log(guess)
+console.log(guess == secret)
