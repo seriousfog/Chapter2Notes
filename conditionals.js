7a08715e5e36446f19285c3aaf384c22d9409136
t9uo <t9uo@fresnou.org>
2023-02-07 09:47:09 -0800

    initial commit

---
+++ conditionals.js
@@ -0,0 +1,20 @@
+let x = 5;
+if (x == 5) {
+    console.log("it's the number five!");
+}
+console.log("end");
+
+let y = 10;
+if (y == 5){
+    console.log("it's the number five");
+}
+console.log("end of program")
+
+let z = 30;
+if (z == 5){  //option 1 = 30, option 2 = any other number that does not equal 30
+
+    console.log("option1");
+} else {
+    console.log("option2");
+}
+console.log("end of program");
