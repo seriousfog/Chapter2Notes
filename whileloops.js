9c9ea8a03e9ece052af3c8c490c892229caaa182
t9uo <t9uo@fresnou.org>
2023-02-17 10:12:17 -0800

    initial commit

---
+++ whileloops.js
@@ -0,0 +1,12 @@
+const readLine = require('readline-sync');
+
+
+let start = 0;
+while (start <= 100){
+    if(start < 10){
+        console.log(start);
+    } else if (start > 25){
+        console.log(start);
+    }
+    start++;
+}
---
+++ whileloops1.js
@@ -0,0 +1,9 @@
+const readLine = require('readline-sync');
+
+
+//4
+let x = 0;
+while (x < -50 ){
+    console.log(x);
+    x--;
+}
