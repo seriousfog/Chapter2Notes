cf019718202e03f5a74e60df2fdf35f480092677
t9uo <t9uo@fresnou.org>
2023-02-23 10:28:47 -0800

    initial commit

---
+++ forloops.js
@@ -0,0 +1,38 @@
+// for loops
+// for (let i = 0; i <= 9; i++){
+    //console.log("hi")
+// }
+
+// for (let i = 0; i <= 9; i++){
+// console.log(i)
+// }
+
+// for (let i = 0; i <= 1000; i++){
+// console.log(i)
+// }
+
+// for (let i = 0; i <= 1000; i--){
+// console.log(i)
+// }
+
+// for (let i = 100; i <= 300; i++){
+//     if (i % 2 == 0){
+//         console.log(i)
+//     }
+// }
+
+// let sum = 0;
+// for (let i = 0; i <= 100; i++){
+//     sum += i;
+// }
+// console.log(sum)
+
+//even numbers
+let sum = 0;
+for (let i = 0; i <= 100; i++){
+    if (i % 2 == 0){
+        sum += i;
+    }
+}
+console.log(`sum: ${sum}`);
+
---
+++ forloops1.js
@@ -0,0 +1,57 @@
+
+
+//1
+// for (let i = 0; i <= 150; i++){
+// console.log(i)
+//}
+
+//2
+// for (let i = 0; i <= 40; i++){
+//          console.log(i**2)
+//}
+
+//3
+// for (let i = 0; i <= 30; i++){
+//           console.log(i**3)
+//}
+
+//4
+// for (let i = 0; i <= 700; i++){
+//     if (i % 5 == 0){
+//         console.log(i)
+//   }
+//}
+
+//5
+// for (let i = 350; i <= 1000; i++){
+//      if (i % 10 == 0){
+//          console.log(i)
+//    }
+// }
+
+//6
+//  let sum = 0;
+//  for (let i = 1; i <= 50; i++){
+//      sum += i;
+//  }
+//  console.log(sum)
+
+//7
+// let sum = 0;
+//   for (let i = 35; i <= 70; i++){
+//       sum += i;
+//   }
+//   console.log(sum)
+
+//8
+// let sum = 0;
+// for (let i = 1; i <= 15; i++){
+//     sum **= i;
+// }
+// console.log(sum)
+
+//10
+ for (let i = 0; i <= 12; i++){
+console.log("Happy Holidays!")
+ }
+
