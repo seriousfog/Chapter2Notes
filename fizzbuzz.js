a5b60f5f28949af84b98d5510958d46c2d8eb0d3
t9uo <t9uo@fresnou.org>
2023-03-07 10:29:01 -0800

    initial commit

---
+++ fizzbuzz.js
@@ -0,0 +1,36 @@
+const readLine = require ('readline-sync');
+
+//forloops
+// for (let i=1; i <= 100; i++)
+// {
+//  if (i % 15 == 0)
+//     console.log('fizzbuzz');
+//  else if (i % 3 == 0)
+//      console.log('fizz');
+//
+// else if (i % 5 == 0)
+//     console.log('buzz');
+//
+// else
+//     console.log(i);
+// }
+
+//whileloops
+// var i = 1 ;
+// while (i <= 100) {
+//     if( i % 3 === 0 ) {
+//         console.log( 'fizz' );
+//
+//     }
+//     else if( i % 5 === 0 ) {
+//         console.log( 'buzz' );
+//     }
+//     else if( i % 3 === 0 && i % 5 === 0 ) {
+//
+//         console.log( 'buzzfizz' );
+//     }
+//     else{
+//         console.log(i);
+//     }
+//     i++;
+// }
--- forloops1.js
+++ forloops1.js
@@ -8,7 +8,7 @@ const readLine = require('readline-sync');
 //2
 // for (let i = 0; i <= 40; i++){
 //          console.log(i**2)
-//}
+// }
 
 //3
 // for (let i = 0; i <= 30; i++){
